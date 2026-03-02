c6a33ae8745e9570802f5309b77c52fc2fa0152b
dodeca-6-tope <21145501+dodeca-6-tope@users.noreply.github.com>
2026-03-02 11:07:05 +0200

    move fzf config to its own file

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,6 +3,3 @@ alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 # Git
 alias g='git'
-
-# FZF
-export FZF_DEFAULT_OPTS="--layout reverse --no-separator --border none"
---
+++ .oh-my-zsh/custom/fzf.zsh
@@ -0,0 +1,2 @@
+# FZF
+export FZF_DEFAULT_OPTS="--layout reverse --no-separator --border none"
