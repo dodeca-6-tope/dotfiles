f3ba8dd33a1c2a375229a4ae621c7b42c8ffd327
dodeca-6-tope <21145501+dodeca-6-tope@users.noreply.github.com>
2026-03-02 11:02:08 +0200

    remove p command (replaced by jora)

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -7,36 +7,6 @@ alias g='git'
 # FZF
 export FZF_DEFAULT_OPTS="--layout reverse --no-separator --border none"
 
-# PR dashboard
-_p_list() {
-  git rev-parse --git-dir &>/dev/null || return 0
-  gh pr list --search "involves:@me" \
-    --json number,url,title,author,statusCheckRollup,reviewDecision \
-  | jq -r '.[] |
-    def pad(n): (. + "               ")[:n];
-
-    (.reviewDecision | if . == "APPROVED" then "\u001b[32m●"
-      elif . == "CHANGES_REQUESTED" then "\u001b[31m●"
-      else "\u001b[90m○" end) as $rv |
-    (.statusCheckRollup | if length == 0 then "\u001b[90m□"
-      elif all(.conclusion == "SUCCESS") then "\u001b[32m■"
-      elif any(.conclusion == "FAILURE") then "\u001b[31m■"
-      else "\u001b[33m■" end) as $ci |
-
-    "\(.number)\t\(.url)\t\u001b[32m\(.number | tostring | pad(5))\u001b[0m \($rv) \($ci)\u001b[0m\t\u001b[36m\(.author.login | pad(15))\u001b[0m\t\(.title)"
-  '
-}
-
-p() {
-  _p_list | fzf --tmux 100%,100% \
-    --ansi --delimiter='\t' --with-nth=3.. --tabstop=1 --no-hscroll \
-    --header "⏎ view | ^d diff | ^o checkout | ^r refresh" \
-    --bind 'enter:execute-silent(open {2})' \
-    --bind 'ctrl-d:execute(git dp {1})' \
-    --bind 'ctrl-o:become(gh pr checkout {1})' \
-    --bind "ctrl-r:reload(zsh -c 'source ~/.oh-my-zsh/custom/aliases.zsh && _p_list')"
-}
-
 # Download files from Coder workspace
 cdl() {
   local workspace selected
